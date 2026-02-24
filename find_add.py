9186c31b71065b7447d5cf7e2b3acfb7f88190a1
Prathamesh Pawar <pawarpa05@gmail.com>
2026-02-24 22:18:23 +0530

    Update find_add.py
    Refactored code to encapsulate logic in step_ans function.

--- find_add.py
+++ find_add.py
@@ -1,7 +1,10 @@
+def step_ans(x):
+     l=[1,2,3,4,5,6,7,8,9,10]
+     for i in range(0,len(l)):
+          for j in range(1,len(l)):
+              if l[i] + l[j] == x:
+                  print(f"The two numbers are {l[i]} and {l[j]}") 
 l=[1,2,3,4,5,6,7,8,9,10]
 print(l)
-x=int(input("Enter Sum You Want:"))
-for i in range(0,len(l)):
-     for j in range(1,len(l)):
-         if l[i] + l[j] == x:
-             print(f"The two numbers are {l[i]} and {l[j]}") 
+num=int(input("Enter Sum You Want:"))
+step_ans(num)
