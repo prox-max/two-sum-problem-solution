0b7152b52c3d53edf91c5289edd73c0931ba1f70
Prathamesh Pawar <pawarpa05@gmail.com>
2026-02-24 21:37:04 +0530

    Add files via upload

---
+++ find_add.py
@@ -0,0 +1,7 @@
+l=[1,2,3,4,5,6,7,8,9,10]
+print(l)
+x=int(input("Enter Sum You Want:"))
+for i in range(0,len(l)):
+     for j in range(1,len(l)):
+         if l[i] + l[j] == x:
+             print(f"The two numbers are {l[i]} and {l[j]}") 
